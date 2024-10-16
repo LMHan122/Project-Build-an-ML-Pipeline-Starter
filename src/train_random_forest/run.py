21e0eef140eb598869894626f2d4300d3af3cdb4
Leslie Hanson <lesliemhanson@gmail.com>
2024-10-16 15:33:49 -0500

    adding code to train random forest in run.py

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -71,10 +71,14 @@ def go(args):
     # Then fit it to the X_train, y_train data
     logger.info("Fitting")
 
+
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+    
+    sk_pipe.fit(X_train, y_train)
     ######################################
+
 
     # Compute r2 and MAE
     logger.info("Scoring")
@@ -95,9 +99,12 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
+    export_path = "random_forest_dir"
     signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
         # YOUR CODE HERE
+        sk_pipe,
+        export_path,
         signature = signature,
         input_example = X_train.iloc[:5]
     )
@@ -122,6 +129,7 @@ def go(args):
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
     # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -165,6 +173,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"), OrdinalEncoder()
     )
     ######################################
 
